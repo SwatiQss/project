8f07612016dc8379172155d9427216e81fb34d2a
Swati Tiwari <swati.tiwari@qsstechnosoft.com>
2025-01-20 15:05:03 +0530

    tsx

---
+++ my-app/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    
+       <div className='merge-page'>
+     <p className='heading'>Master Name Index</p>
+     <h3 className='sub-heading'>Master Name Record Merge</h3>
+    </div>
+      
+    
+  );
+}
